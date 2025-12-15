c712f4abace81ad4a7f453f56eb2353a9e5c197c
msetzu <setzu.mattia@gmail.com>
2025-12-15 16:25:01 +0100

    che cosa ho letto wtf

--- programmazione/esercitazioni/es5.py
+++ programmazione/esercitazioni/es5.py
@@ -1,6 +1,15 @@
 def apply_mask(collection: list, mask: list):
     """Applica la maschera mask a collection, restituendone i valori selezionati."""
-    return [collection[mask_filter] for mask_filter in mask if mask_filter]
+    # versione 1 (stampa e basta)
+    # for i in range(len(collection)):
+    #     if mask[i]:
+    #         print(collection[i])
+
+    # versione 2 (li ritorna)
+    return [element for element, mask_filter in zip(collection, mask) if mask_filter]
+
+    # versione 3 (li ritorna)
+    return [collection[i] for i in range(len(collection)) if mask[i]]
 
 def apply_negated_mask(collection: list, mask: list):
     """Applica la maschera mask a collection, restituendone i valori *non* selezionati."""
@@ -10,3 +19,7 @@ def sum_mask(collection: list, mask: list) -> float:
     """Somma tutti i valori selezionati dalla maschera."""
     return sum(apply_mask(collection, mask))
 
+print(apply_mask(
+    ["A", "B", "C", "D"],
+    [True, False, True, False]
+))
